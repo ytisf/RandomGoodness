a5a6b5f21ae22fdb360a6a83e678d2cb17c4fe2a
Yuval Nativ <ytisf@users.noreply.github.com>
2014-11-11 23:14:49 +0200

    Create foca_extractor.py
    Just to allow extraction of metadata from a FOCA file.

---
+++ foca_extractor.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+
+import os
+import zipfile
+
+FILENAME = 'meitavdash.co.il.FOCA'
+READONLY = 'r'
+WRITE_BINARY = 'wb'
+CURRENT_DIR = '.'
+TEMP_DIR = 'temp/'
+EXTRACT_ARRAY = [["Emails", "metadatasummaryemails"],
+				["Folders", "metadatasummaryfolders"],
+				["OSs", "metadatasummaryoperatingsystems"],
+				["Printers", "metadatasummaryprinters"],
+				["Software", "metadatasummarysoftware"],
+				["Users", "metadatasummaryusers"]]
+
+DEL_ARRAY = ["documents","metadatasummaryemails", "metadatasummaryfolders",
+				"metadatasummaryoperatingsystems", "metadatasummaryprinters",
+				"metadatasummarysoftware", "metadatasummaryusers",
+				"[Content_Types].xml" ]
+
+# Extract
+with zipfile.ZipFile(FILENAME, READONLY) as zh:
+	zh.extractall(".")
+
+# Extract for each
+for title, filename in EXTRACT_ARRAY:
+	fh_output = open(title+".txt", WRITE_BINARY)
+	fh_input = open(filename, READONLY)
+
+	all_lines = fh_input.readlines()
+	for line in all_lines:
+		if line.find('<string>') != -1:
+			fh_output.write(line[14:-11] + '\n')
+
+	fh_output.close()
+	fh_input.close()
+
+
+# Cleanup
+for del_file in DEL_ARRAY:
+	os.system('rm ' + del_file)
